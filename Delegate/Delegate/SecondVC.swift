45d051af743eebf0b0a12e48295070e77ec7c5a1
Boss <luyenddgc00774@gmail.com>
2019-09-04 11:34:02 +0700

    add secondVC

---
+++ Delegate/Delegate/SecondVC.swift
@@ -0,0 +1,30 @@
+//
+//  SecondVC.swift
+//  Delegate
+//
+//  Created by Boss on 9/4/19.
+//  Copyright © 2019 LuyệnĐào. All rights reserved.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
